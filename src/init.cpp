72650093e4832d1e35d3a1da4bf91ec2508de243
NotRin7 <102989232+NotRin7@users.noreply.github.com>
2024-04-09 15:32:57 +0200

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -579,7 +579,7 @@ void SetupServerArgs()
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/palladium/palladium>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/NotRin7/palladium>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
